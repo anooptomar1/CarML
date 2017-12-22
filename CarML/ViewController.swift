dd59595558d22ad8602c835878e124332a054682
David Garcia <david.a.g.r93@gmail.com>
2017-12-22 15:13:35 -0400

    Pantalla de precio 	modified:   CarML/Base.lproj/Main.storyboard 	modified:   CarML/Info.plist 	modified:   CarML/ViewController.swift

--- CarML/ViewController.swift
+++ CarML/ViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var modelSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var extrasSwitch: UISwitch!
+    @IBOutlet weak var kmsLabel: UILabel!
+    @IBOutlet weak var kmsSlider: UISlider!
+    @IBOutlet weak var statusSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var priceLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +32,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func calculateValue() {
+        
+    }
+    
 }
 
